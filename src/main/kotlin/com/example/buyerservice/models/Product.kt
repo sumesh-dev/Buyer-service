f7f25768cc87daea0823f5a65a402a6cd64ce28e
sumesh <sumeshkumarkuchya@gmail.com>
2022-01-24 14:47:38 +0530

    resolve feign client issue

--- src/main/kotlin/com/example/buyerservice/models/Product.kt
+++ src/main/kotlin/com/example/buyerservice/models/Product.kt
@@ -4,8 +4,9 @@ import org.bson.types.ObjectId
 
 data class Product(
     var _id: String,
-     var name:String?,
-    var img:String?,
-    var price: Int?,
-    var desc: String?,
-    var addBy: String?)
+     var name:String,
+    var img:String,
+    var price: Int,
+    var desc: String,
+    var addBy: String,
+    var review: MutableList<Review>)
